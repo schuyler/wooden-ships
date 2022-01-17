c3d3f2171d9791029763aa5fb9f1a40f88d9839c
Schuyler Erle <schuyler@lattice.com>
2022-01-16 20:55:17 -0800

    Fix Wind class

--- src/classes/weather.ts
+++ src/classes/weather.ts
@@ -2,9 +2,10 @@ import { Math } from 'phaser';
 
 export const minWindSpeed = 5.0;
 export const maxWindSpeed = 45.0;
-export const windSpeedToPixels = 5;
+export const windSpeedToPixels = 1;
 
 export class Wind extends Math.Vector2 {
+    // This class stores the velocity vector of the wind
     constructor(direction?: number, speed?: number) {
         super(1.0, 1.0);
         this.setDirection(direction ?? Math.Angle.RandomDegrees());
@@ -12,11 +13,13 @@ export class Wind extends Math.Vector2 {
     }
 
     public setSpeed(speed: number) {
-        this.setLength(speed * windSpeedToPixels);
+        this.setLength(Math.Clamp(speed * windSpeedToPixels, minWindSpeed, maxWindSpeed));
     }
 
     public setDirection(direction: number) {
-        this.setAngle(Math.Angle.Normalize(Math.DegToRad(direction)));
+        // Wind direction is the direction the wind is coming FROM --
+        // this is the reverse of the angle of the force vector applied by the wind
+        this.setAngle(Math.Angle.Normalize(Math.Angle.Reverse(Math.DegToRad(direction))));
     }
 
     public speed(): number {
@@ -24,7 +27,8 @@ export class Wind extends Math.Vector2 {
     }
 
     public direction(): number {
-        return this.angle();
+        // Wind angle is reversed here because direction is where the wind is coming FROM, not where it's going
+        return Math.RadToDeg(Math.Angle.Reverse(this.angle()));
     }
 
     public relativeTo(velocity: Math.Vector2) {
@@ -32,5 +36,9 @@ export class Wind extends Math.Vector2 {
         apparentWind.setFromObject(this);
         apparentWind.subtract(velocity);
         return apparentWind;
+    }
+
+    public angleOfAttack(velocity: Math.Vector2) {
+        return Math.Angle.Wrap(Math.Angle.Reverse(this.angle()) - velocity.angle());
     }
 }
